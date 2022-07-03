e24ac99cef00188bfaf3867155eea21cafad171d
Jason Siefken <siefkenj@gmail.com>
2022-07-02 20:42:34 -0400

    Update tests

--- test/format.test.ts
+++ test/format.test.ts
@@ -5,59 +5,60 @@ import prettier from "prettier";
 import { Options } from "../src/types";
 import plugin from "../src/plugin";
 
-const fixture = fs.readFileSync(path.join(__dirname, "./fixture.xml"), "utf-8");
+const fixture = fs.readFileSync(path.join(__dirname, "./fixture.ptx"), "utf-8");
+const example2 = fs.readFileSync(
+    path.join(__dirname, "./example2.ptx"),
+    "utf-8"
+);
 
 function format(content: string, opts: Partial<Options> = {}) {
-  return prettier.format(content, {
-    ...opts,
-    parser: "ptx",
-    plugins: [plugin as any as string] // hacky but it works
-  });
+    return prettier.format(content, {
+        ...opts,
+        parser: "ptx",
+        plugins: [plugin as any as string], // hacky but it works
+    });
 }
 
 test("defaults", () => {
-  const formatted = format(fixture);
-  expect(formatted).toMatchSnapshot();
+    const formatted = format(fixture);
+    expect(formatted).toMatchSnapshot();
 });
 
-test("xmlWhitespaceSensitivity => ignore", () => {
-  const formatted = format(fixture, { xmlWhitespaceSensitivity: "ignore" });
-  expect(formatted).toMatchSnapshot();
+test("basic pretext source", () => {
+    const formatted = format(example2);
+    expect(formatted).toMatchSnapshot();
 });
+
 
 test("bracketSameLine => true", () => {
-  const formatted = format(fixture, {
-    bracketSameLine: true,
-    xmlWhitespaceSensitivity: "ignore"
-  });
+    const formatted = format(fixture, {
+        bracketSameLine: true,
+    });
 
-  expect(formatted).toMatchSnapshot();
+    expect(formatted).toMatchSnapshot();
 });
 
 test("xmlSelfClosingSpace => false", () => {
-  const formatted = format(fixture, {
-    xmlSelfClosingSpace: false,
-    xmlWhitespaceSensitivity: "ignore"
-  });
+    const formatted = format(fixture, {
+        xmlSelfClosingSpace: false,
+    });
 
-  expect(formatted).toMatchSnapshot();
+    expect(formatted).toMatchSnapshot();
 });
 
 test("bracketSameLine => true, xmlSelfClosingSpace => false", () => {
-  const formatted = format(fixture, {
-    bracketSameLine: true,
-    xmlSelfClosingSpace: false,
-    xmlWhitespaceSensitivity: "ignore"
-  });
+    const formatted = format(fixture, {
+        bracketSameLine: true,
+        xmlSelfClosingSpace: false,
+    });
 
-  expect(formatted).toMatchSnapshot();
+    expect(formatted).toMatchSnapshot();
 });
 
 test("singleAttributePerLine => true", () => {
-  const formatted = format(fixture, {
-    singleAttributePerLine: true,
-    xmlWhitespaceSensitivity: "ignore"
-  });
+    const formatted = format(fixture, {
+        singleAttributePerLine: true,
+    });
 
-  expect(formatted).toMatchSnapshot();
+    expect(formatted).toMatchSnapshot();
 });
